c46ce9c7a6156ca8239268a927eb9c7b7a954ac9
eltesoro-dnn <eleanor.tesoro@dnnsoftware.com>
2016-02-12 17:34:04 -0800

    Fixed bugs with dirs in bld. Experiments with sidebar.

--- _build/bld.bat
+++ _build/bld.bat
@@ -8,13 +8,14 @@ if "%OS%"=="WINNT" @setlocal
 :. ----- Modify the environment settings below as needed. -----
 
 :. Choose one output format or specify it in as the first parameter to this batch file.
-:. Valid options: common-html, docbook, eclipsecontent, eclipsehelp, html5, htmlhelp, javahelp, odt, pdf, pdf2, tocjs, troff, wordrtf, xhtml.
+:. Currently available: html5, xhtml.
+:. Future options: common-html, docbook, eclipsecontent, eclipsehelp, htmlhelp, javahelp, odt, pdf, pdf2, tocjs, troff, wordrtf.
 set _transtype=html5
-for %%v in ( common-html docbook html5 htmlhelp pdf pdf2 xhtml ) do  if _%1_ EQU _%%v_  set _transtype=%1
+for %%v in ( html5 xhtml ) do  if _%1_ EQU _%%v_  set _transtype=%1
 
 if _%_gitdir%_      EQU __    set _gitdir=w:\.
 if _%_blddir%_      EQU __    set _blddir=v:\bld
-if _%_tgtdir%_      EQU __    set _tgtdir=v:\output\%_transtype%
+if _%_outdir%_      EQU __    set _outdir=v:\output
 if _%_logdir%_      EQU __    set _logdir=v:\logs
 
 if _%DITA_HOME%_    EQU __    set DITA_HOME=C:\dita-ot
@@ -30,30 +31,29 @@ if exist %JAVA_HOME%\bin\java.exe  set JAVACMD=%JAVA_HOME%\bin\java.exe
 call "%DITA_HOME%\resources\env.bat"
 
 
-echo Deleting files in build directory %_blddir% ....
-rd /s/q %_blddir%  >nul
-md %_blddir%
+echo Deleting build folder, output folder, and logs folder....
+for %%v in ( %_blddir% %_outdir% %_logdir% ) do rd /s/q %%v >nul
+
 
 echo Copying files required by the build ....
-xcopy %_gitdir%\_content\*.dita*         %_blddir%\.                /s/v/y
-xcopy %_gitdir%\_content\*.png           %_blddir%\.                /s/v/y
+xcopy %_gitdir%\_content\*.dita*         %_blddir%                /i/s/v/y
+xcopy %_gitdir%\_content\*.png           %_blddir%                /i/s/v/y
 xcopy %_gitdir%\_content\common\samples  %_blddir%\common\samples /i/s/v/y
 xcopy %_gitdir%\_themes\dnn\dnn*.css     %_blddir%\_themes\dnn    /i/s/v/y
 
 
 echo Integrating our own DITA-OT plugin ....
-rd /s/q %DITA_HOME%\plugins\org.dnn.dc
+rd /s/q %DITA_HOME%\plugins\org.dnn.dc >nul
 xcopy %_gitdir%\_build\org.dnn.dc\*.*  %DITA_HOME%\plugins\org.dnn.dc  /i/s/v/y
 cd /d   %DITA_HOME%
 call ant -f integrator.xml strict
 
 
 echo bld.bat: Building %_transtype% ....
-md %_tgtdir%
-md %_logdir%
 
 :. Must start at %DITA_HOME%
 cd /d %DITA_HOME%
+md %_logdir% > nul
 echo. > %_logdir%\log.txt
 xcopy %_gitdir%\_build\dnn_build.xml  %DITA_HOME%\.  /v/y
 call ant %_transtype% -f %DITA_HOME%\dnn_build.xml -l %_logdir%\log.txt
@@ -61,13 +61,13 @@ pause
 
 
 echo Copying additional files required to the output ....
-xcopy %_gitdir%\_content\common\samples   %_tgtdir%\common\samples   /i/s/v/y
-xcopy %_gitdir%\_content\common\img\*.png %_tgtdir%\common\img       /i/s/v/y
-xcopy %_gitdir%\_themes\dnn\26D3F6_6_0.*  %_tgtdir%\_theme           /i/s/v/y
-xcopy %_gitdir%\_themes\dnn\*.jpg         %_tgtdir%\_theme           /i/s/v/y
-xcopy %_gitdir%\_themes\dnn\*.js          %_tgtdir%\_theme           /i/s/v/y
+xcopy %_gitdir%\_content\common\samples   %_outdir%\%_transtype%\common\samples   /i/s/v/y
+xcopy %_gitdir%\_content\common\img\*.png %_outdir%\%_transtype%\common\img       /i/s/v/y
+xcopy %_gitdir%\_themes\dnn\26D3F6_6_0.*  %_outdir%\%_transtype%\_theme           /i/s/v/y
+xcopy %_gitdir%\_themes\dnn\*.jpg         %_outdir%\%_transtype%\_theme           /i/s/v/y
+xcopy %_gitdir%\_themes\dnn\*.js          %_outdir%\%_transtype%\_theme           /i/s/v/y
 
 
-:. runas /user:administrator /profile "%~f0\v2iis.bat %_tgtdir% c:\inetpub\wwwroot"
-start %_tgtdir%
+:. runas /user:administrator /profile "%~f0\v2iis.bat %outdir%\%_transtype% c:\inetpub\wwwroot"
+start %_outdir%\%_transtype%
 start c:\inetpub\wwwroot
--- _build/org.dnn.dc/xsl/sidetoc.xsl
+++ _build/org.dnn.dc/xsl/sidetoc.xsl
@@ -4,7 +4,22 @@
     <xsl:template name="gen-user-sidetoc">
 
         <div class="sidetoc">
-            <p>Temporary side panel.</p>
+            <nav id="main-navigation" class="navigation">
+                <a href="#">Link1</a>
+                <a href="#">Link2</a>
+                <a href="#">Link3</a>
+                <a href="#">Link4</a>
+                <a href="#">Link5</a>
+                <a href="#">Link6</a>
+            </nav>
+        </div>
+
+        <div class="page-wrap">
+            <header>
+                <a href="#main-navigation">Menu</a>
+                <a href="#">Close Menu</a>
+            </header>
+            <!-- content -->
         </div>
 
     </xsl:template>
