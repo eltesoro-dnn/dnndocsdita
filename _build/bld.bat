ed8392809fef79efd8f36f2d8f0a4fedb71e827b
Eleanor Tesoro <eleanor.tesoro@dnnsoftware.com>
2017-03-16 16:41:51 -0700

    * For Support, extracted some build settings from bld.ps1 and moved it to bld.bat. * In bld.ps1 ** Implemented the auto-copy of built files to c:\inetpub\wwwrooot\docs, by invoking an elevated cmd.exe session. (Hack that invokes a temporarybatch file for removing tree and robocopying files, instead of doing folder manipulations inside Powershell.) ** Moved some settings out, and moved other settings to the main section of the script. Deleted SetBldEvars. Other tweaks. ** Updated TestPath to allow throwing an exception. ** Replaced 7z with built-in Expand-Archive cmdlet. ** Updated RunBat function to allow multiple lines in the temp batch file. * Per ChrisP, created dnn-security.dita based on http://www.dnnsoftware.com/wiki/security. * Reorganized the order of topics in configuring-your-site.ditamap. * Additional images that might be used in the future. Completed configure-html-editor.dita without the CKEditor topic.

---
+++ _build/bld.bat
@@ -0,0 +1,31 @@
+@echo off
+cls
+
+:. Set to blank if you don't care about zipping the build files.
+set _zipoutdir=
+
+:. Path to the text editor of your choice.
+set _texteditor=%ProgramFiles(x86)%\Notepad++\notepad++.exe
+
+:. Path to the docs folder in your local website.
+set _localhostdocdir=c:\inetpub\wwwroot\docs
+
+:. Mapped drive to the build folder.
+set _bldroot=v:
+
+:. Mapped drive to the source files.
+set _gitroot=w:
+
+:. Java JDK version. Keep the same format.
+set _javajdkver=jdk1.8.0_121
+
+:. Path to Java folder.
+set JAVA_HOME=%ProgramFiles%\Java\%_javajdkver%
+
+:. Path to DITA-OT. The files will be extracted here.
+set DITA_HOME=C:\dita-ot
+
+
+
+:. Call to PS script
+powershell -file %_gitroot%\_build\bld.ps1
